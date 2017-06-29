0913b37d72c9b9a7326ddf9dd7fd10f21af8d942
ashwini03 <ashwini.naganur@yahoo.in>
2017-06-29 04:27:49 +0000

    used attributes in default.rb

--- cookbooks/cookbook_test/recipes/default.rb
+++ cookbooks/cookbook_test/recipes/default.rb
@@ -5,13 +5,22 @@
 # Copyright 2017, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-package "httpd" do
+
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platform_family'] == "debian"
+	package == "apache2"
+end
+
+package "apache2"  do
+	package_name package
 	action :install
 end
-service "httpd" do
+service "apache2" do
+	service_name "httpd"
 	action [:enable, :start]
 end
-template "/var/www/html/index.html" do
-	source "index.html.erb"
-	mode "0644"
-end
+#template "/var/www/html/index.html" do
+#	source "index.html.erb"
+#	mode "0644"
+#end
